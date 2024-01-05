50af4aa1ff3e821af41c57e2d65eda3383847d84
kharempsm <kharempsm@gmail.com>
2024-01-05 12:20:09 -0300

    hicimos un pequeño cambio

--- src/Componentes/NavBar/NavBar.jsx
+++ src/Componentes/NavBar/NavBar.jsx
@@ -7,9 +7,9 @@ const NavBar = () => {
       <h1 className="header__titulo">Kp Store</h1>
       <nav>
         <ul>
-          <li>Hombre</li>
-          <li>Mujer</li>
-          <li>Accesorios</li>
+          <li>Hombre.</li>
+          <li>Mujer.</li>
+          <li>Accesorios.</li>
         </ul>
       </nav>
       <CardWidget />
